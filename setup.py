606ee21ba97018b8d1499129ce880197ef2a0553
RozaliaSolecka <62565128+RozaliaSolecka@users.noreply.github.com>
2022-04-14 20:40:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,4 @@ from distutils.core import setup, Extension
 
 if __name__ == '__main__':
 
-    setup(name="simple_graphs", version="1.0",
-          ext_modules=[Extension("simple_graphs", ["SimpleGraphs.cpp"])])
+    setup(name="simple_graphs", version="1.0", ext_modules=[Extension("simple_graphs", ["SimpleGraphs.cpp"])])
